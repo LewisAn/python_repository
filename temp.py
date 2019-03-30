711948165a524919375951961a29538582e6402b
lewis <s4457600@student.uq.edu.au>
2019-03-30 11:09:20 +1000

    add this csv reader to repo

---
+++ temp.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import csv
+from matplotlib import pyplot as p
+from datetime import datetime
+
+filename = 'sitka_weather_07-2014.csv'
+
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+    temp = next(reader)
+    highs = []
+    dates = []
+    for row in reader:
+        current_date = datetime.strptime(row[0], "%Y-%m-%d")
+        highs.append(int(row[6]))
+        dates.append(current_date)
+#    print(highs)
+    fig = p.figure(dpi=128, figsize=(10, 6))
+    p.plot(dates, highs, c='red')
+    p.title("Temp")
+    fig.autofmt_xdate()
+    p.xlabel("", fontsize=16)
+    p.ylabel("Temperature (f)", fontsize=10)
+    p.tick_params(axis="both", which='major', labelsize=16)
+    p.savefig('2.png')
+    
