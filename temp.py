9e08991b496973826203e3c650472485a605c612
lewis <s4457600@student.uq.edu.au>
2019-03-30 11:22:38 +1000

    update figures

--- temp.py
+++ temp.py
@@ -14,20 +14,21 @@ filename = 'sitka_weather_07-2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    temp = next(reader)
+#    temp = next(reader)
     highs = []
-    dates = []
+#    dates = []
     for row in reader:
-        current_date = datetime.strptime(row[0], "%Y-%m-%d")
+#        current_date = datetime.strptime(row[0], "%Y-%m-%d")
         highs.append(int(row[6]))
-        dates.append(current_date)
-#    print(highs)
+#        dates.append(current_date)
+    print(highs)
+#    print(dates)
     fig = p.figure(dpi=128, figsize=(10, 6))
-    p.plot(dates, highs, c='red')
+    p.plot(highs, c='red')
     p.title("Temp")
-    fig.autofmt_xdate()
+#    fig.autofmt_xdate()
     p.xlabel("", fontsize=16)
     p.ylabel("Temperature (f)", fontsize=10)
     p.tick_params(axis="both", which='major', labelsize=16)
-    p.savefig('2.png')
+#    p.savefig('2.png')
     
